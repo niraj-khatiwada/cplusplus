72977ed802fc72a0dde04cf3864ec9518447f67e
niraj-khatiwada <nirazzz.nk@gmail.com>
2025-12-29 22:26:13 +0545

    feat: reference

--- src/pointers.cpp
+++ src/pointers.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 
-int main()
-{
-    std::string name = "Niraj";
-    std::string *pName = &name;
+int main() {
+  std::string name = "Niraj";
+  std::string* pName = &name;
 
-    std::cout << name << "\n";
-    std::cout << *pName << "\n";
+  std::cout << name << "\n";
+  std::cout << *pName << "\n";
 
-    *pName = "Suraj";
+  *pName = "Suraj";
 
-    std::cout << name << "\n";
-    std::cout << *pName << "\n";
+  std::cout << name << "\n";
+  std::cout << *pName << "\n";
 
-    std::string foods[2] = {"A", "B"};
-    std::string *pFoods = foods; // we don't need to give & here since array is already a pointer
+  std::string foods[2] = {"A", "B"};
+  std::string* pFoods =
+      foods; // we don't need to give & here since array is already a pointer
 
-    pFoods[1] = "C";
+  pFoods[1] = "C";
 
-    for (int i = 0; i < 2; i++)
-    {
-        std::cout << foods[i] << "\n";
-        std::cout << pFoods[i] << "\n";
-    }
+  for (int i = 0; i < 2; i++) {
+    std::cout << foods[i] << "\n";
+    std::cout << pFoods[i] << "\n";
+  }
 }
